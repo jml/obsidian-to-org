329524293d7f76123ba46db18ad37f04bd11b589
Jonathan Lange <jml@mumak.net>
2022-06-03 08:10:46 +0100

    Rename compiled regexes to match my conventions
    Also change "comm" to "comment" for easier reading

--- obsidian-to-org.py
+++ obsidian-to-org.py
@@ -27,12 +27,12 @@ def convert_markdown_file(md_file, output_dir):
     org_file = (output_dir / md_file.stem).with_suffix(".org")
 
     # Treat all comments in file
-    re_comm = re.compile(r"^%%(.*?)%%", re.MULTILINE)
-    replace(re_comm, r"#!#comment: \1", md_file)
+    obsidian_comment_re = re.compile(r"^%%(.*?)%%", re.MULTILINE)
+    replace(obsidian_comment_re, r"#!#comment: \1", md_file)
 
     # Ensure space after "---"
-    re_ruler = re.compile(r"^---\n(.+)", re.MULTILINE)
-    replace(re_ruler, r"---\n\n\1", md_file)
+    ruler_re = re.compile(r"^---\n(.+)", re.MULTILINE)
+    replace(ruler_re, r"---\n\n\1", md_file)
 
     # Convert from md to org
     pandoc_command = (
@@ -41,13 +41,13 @@ def convert_markdown_file(md_file, output_dir):
     os.system(pandoc_command)
 
     # Regularize comments
-    re_comm_org = re.compile(r"^#!#comment:(.*?)$", re.MULTILINE)
-    replace(re_comm_org, r"#\1", org_file)
+    org_comment_re = re.compile(r"^#!#comment:(.*?)$", re.MULTILINE)
+    replace(org_comment_re, r"#\1", org_file)
 
     # Convert all kinds of links
-    re_url = re.compile(r"\[\[(.*?)\]\[(.*?)\]\]")
-    re_link = re.compile(r"\[\[(.*?)\]\]")
-    re_link_description = re.compile(r"\[\[(.*?)\|(.*?)\]\]")
+    url_re = re.compile(r"\[\[(.*?)\]\[(.*?)\]\]")
+    link_re = re.compile(r"\[\[(.*?)\]\]")
+    link_description_re = re.compile(r"\[\[(.*?)\|(.*?)\]\]")
 
     with open(org_file, "r+") as f:
         content = f.read()
@@ -60,19 +60,19 @@ def convert_markdown_file(md_file, output_dir):
             m_string = m.group(0)
             if "://" in m_string:
                 new_content = (
-                    new_content + content[pos:s] + re.sub(re_url, r"[[\1][\2]]", m_string)
+                    new_content + content[pos:s] + re.sub(url_re, r"[[\1][\2]]", m_string)
                 )
             elif "|" in m_string:
                 new_content = (
                     new_content
                     + content[pos:s]
-                    + re.sub(re_link_description, r"[[file:\1.org][\2]]", m_string)
+                    + re.sub(link_description_re, r"[[file:\1.org][\2]]", m_string)
                 )
             else:
                 new_content = (
                     new_content
                     + content[pos:s]
-                    + re.sub(re_link, r"[[file:\1.org][\1]]", m_string)
+                    + re.sub(link_re, r"[[file:\1.org][\1]]", m_string)
                 )
 
             pos = e
