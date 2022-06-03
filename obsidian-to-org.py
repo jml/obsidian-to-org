a1debaaf3509b16279e85aa0f71e1786ffde781c
Jonathan Lange <jml@mumak.net>
2022-06-03 06:55:46 +0100

    Run `black`

--- obsidian-to-org.py
+++ obsidian-to-org.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 
-import sys,re,os
+import sys, re, os
 
 if not os.path.isdir("out/"):
     os.mkdir("out/")
 
 md_file = sys.argv[1]
 org_file = md_file[:-3] + ".org"
+
 
 def replace(pattern, substitution, filename):
     f = open(filename, "r+")
@@ -17,6 +18,7 @@ def replace(pattern, substitution, filename):
     f.truncate()
     f.close()
 
+
 # Treat all comments in file
 re_comm = re.compile(r"^%%(.*?)%%", re.MULTILINE)
 replace(re_comm, r"#!#comment: \1", md_file)
@@ -26,8 +28,10 @@ re_ruler = re.compile(r"^---\n(.+)", re.MULTILINE)
 replace(re_ruler, r"---\n\n\1", md_file)
 
 # Convert from md to org
-pandoc_command = 'pandoc -f markdown "{0}" --lua-filter=remove-header-attr.lua'\
-                 ' --wrap=preserve -o out/"{1}"'.format(md_file,org_file)
+pandoc_command = (
+    'pandoc -f markdown "{0}" --lua-filter=remove-header-attr.lua'
+    ' --wrap=preserve -o out/"{1}"'.format(md_file, org_file)
+)
 os.system(pandoc_command)
 
 # Regularize comments
@@ -49,11 +53,21 @@ with open("out/" + org_file, "r+") as f:
         e = m.end()
         m_string = m.group(0)
         if "://" in m_string:
-            new_content = new_content + content[pos:s] + re.sub(re_url, r"[[\1][\2]]", m_string)
+            new_content = (
+                new_content + content[pos:s] + re.sub(re_url, r"[[\1][\2]]", m_string)
+            )
         elif "|" in m_string:
-            new_content = new_content + content[pos:s] + re.sub(re_link_description, r"[[file:\1.org][\2]]", m_string)
+            new_content = (
+                new_content
+                + content[pos:s]
+                + re.sub(re_link_description, r"[[file:\1.org][\2]]", m_string)
+            )
         else:
-            new_content = new_content + content[pos:s] + re.sub(re_link, r"[[file:\1.org][\1]]", m_string)
+            new_content = (
+                new_content
+                + content[pos:s]
+                + re.sub(re_link, r"[[file:\1.org][\1]]", m_string)
+            )
 
         pos = e
     new_content = new_content + content[pos:]
